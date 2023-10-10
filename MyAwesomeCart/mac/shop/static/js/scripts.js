5f85ea573853a3fc9026f2d30a5500c0e694a7b1
ahmadkhan5346 <khansarfraz5346@gmail.com>
2023-10-10 18:59:04 +0530

    create plus minus button and add product using this button

--- MyAwesomeCart/mac/shop/static/js/scripts.js
+++ MyAwesomeCart/mac/shop/static/js/scripts.js
@@ -1,13 +1,15 @@
 
-
+// #36 video about session storage and local storage
 // Find out the cart items from localstorage
 if (localStorage.getItem('cart') == null) {
     var cart = {};
+    console.log('mt dic:', cart)
 }
 
 else {
     cart = JSON.parse(localStorage.getItem('cart'));
     document.getElementById('cart').innerHTML = Object.keys(cart).length;
+    console.log('else:', cart)
     updateCart(cart);
 }
 
@@ -26,19 +28,32 @@ $('.cart').click(function () {
 
     updateCart(cart);
 
-    localStorage.setItem('cart', JSON.stringify(cart));
-    document.getElementById('cart').innerHTML = Object.keys(cart).length;
-
 });
 
 // Add popover to cart. add the html in cart in basic.html
 $('#popcart').popover();
-document.getElementById('popcart').setAttribute('data-content', '<h5>Cart for Your items in my shopping cart</h5>')
+document.getElementById('popcart').setAttribute('data-content', '<h5>Cart for Your items in my shopping cart</h5>');
 
 function updateCart(cart) {
     for (var item in cart) {
         document.getElementById('div' + item).innerHTML = "<button id='minus" + item + "' class='btn btn-primary minus'>-</button> <span id='val" + item + "''> " + cart[item] + " </span> <button id='plus" + item + "' class='btn btn-primary plus'> + </button>";
     }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    document.getElementById('cart').innerHTML = Object.keys(cart).length;
+    console.log(cart)
 }
 
-// If plus or minus button is clicked, change the cart as well as the display value
+// If plus or minus button is clicked, change the cart as well as the display value #44 video
+$('.divpr').on("click", "button.minus", function(){
+    a = this.id.slice(7);
+    cart['pr' + a] = cart['pr' + a] - 1;
+    cart['pr' + a] = Math.max(0, cart['pr' + a]);
+    document.getElementById('valpr' + a).innerHTML = cart['pr' + a];
+});
+
+$('.divpr').on("click", "button.plus", function(){
+    a = this.id.slice(4);
+    b = this.id.slice(6);
+    cart[a] = cart[a] + 1;
+    document.getElementById('valpr' + b).innerHTML = cart[a];
+});
